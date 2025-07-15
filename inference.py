e93aafc4cad04fbb8570fc84ae1e8c9f7056b827
Alessandro Palmas <alessandropalmas.mail@gmail.com>
2025-07-14 21:23:58 -0400

    Working random and distance based policies

--- inference.py
+++ inference.py
@@ -4,6 +4,7 @@ from modifiers.metric_damage import RewardWrapper
 from control_policies.random import Agent as RandomAgent
 from control_policies.distance_based import Agent as ClassicAgent
 from control_policies.deeprl.deeprl import Agent as DeepRlAgent
+import statistics
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -16,7 +17,7 @@ if __name__ == "__main__":
     if opt.n_episodes > 1:
         opt.no_render = True
 
-    env = Environment(render_mode="human")
+    env = Environment(render_mode="human" if not opt.no_render else None)
     env = RewardWrapper(env)
 
     if opt.policy == "random":
@@ -28,24 +29,26 @@ if __name__ == "__main__":
     else:
         raise Exception(f"Unrecognized policy type: {opt.policy}")
 
+    print("==========================")
     obs, info = env.reset()
-    if not opt.no_render:
-        env.render()
-    cumulative_reward = 0
+    env.render()
+    cumulative_reward = [0.0]
     episode_counter = 0
     while episode_counter < opt.n_episodes:
-        if not opt.no_render:
-            env.render()
+        env.render()
         action = agent.get_action(obs)
         obs, reward, terminated, truncated, info = env.step(action)
         done = terminated or truncated
-        cumulative_reward += reward
+        cumulative_reward[episode_counter] += reward
         if done:
-            print("==========================")
-            print(f"Cumulative reward [%Damage]: {cumulative_reward}")
-            if not opt.no_render:
-                frame = env.render()
+            print(f"Ep. # {episode_counter+1} - Cumulative reward [%Damage]: {round(cumulative_reward[episode_counter], 2)}")
+            env.render()
             obs, info = env.reset()
             episode_counter += 1
+            cumulative_reward.append(0.0)
 
     env.close()
+
+    if opt.n_episodes > 1:
+        print("==========================")
+        print(f"Cumulative reward [%Damage]: AVG = {round(statistics.mean(cumulative_reward[:-1]), 2)}, STD = {round(statistics.stdev(cumulative_reward[:-1]), 2)}")
